5889bf1e47501b9c84b7d4d5eed59e768987ea77
quidow <liquid1shadow@gmail.com>
2020-06-25 11:50:29 +0300

    Fixed permission for hotels

--- app/booking/permissions.py
+++ app/booking/permissions.py
@@ -4,6 +4,12 @@ from .models import HotelAdminRelation, Room
 
 class IsAdminOrReadOnly(permissions.BasePermission):
 
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
