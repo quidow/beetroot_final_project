5a8442541ed86f7a9a4f59629a9247997618ae25
quidow <liquid1shadow@gmail.com>
2020-07-17 16:31:36 +0300

    add estimated price for rooms for date interval

--- app/booking/views.py
+++ app/booking/views.py
@@ -1,6 +1,10 @@
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework_extensions.mixins import NestedViewSetMixin
+from rest_framework.response import Response
+
+from django.db import connection
 
 from .models import Amenity, Service, Hotel, HotelPhoto, Room, RoomPhoto, RoomPrice
 from .permissions import IsOwnerOrReadOnly, IsSuperUserOrReadOnly, IsOwnerOrReadOnlyHotelRel, \
@@ -78,6 +82,41 @@ class RoomViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     def perform_create(self, serializer):
         hotel = Hotel.objects.get(pk=self.get_parents_query_dict()['hotel'])
         serializer.save(hotel=hotel)
+
+    @action(detail=True, methods=['get'])
+    def calculate_price(self, request, parent_lookup_hotel, pk=None):
+        if not Room.objects.filter(pk=pk).exists():
+            return Response(f"Room with id {pk} does not exist.", status=status.HTTP_404_NOT_FOUND)
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(f"""SELECT SUM(price)
+                    FROM
+                      (SELECT "date",
+                              first_value(price) OVER (PARTITION BY price_partition
+                                                       ORDER BY "date") AS price
+                       FROM
+                         (SELECT p."date",
+                                 p.price,
+                                 sum(CASE
+                                         WHEN p.price IS NULL THEN 0
+                                         ELSE 1
+                                     END) OVER (
+                                                ORDER BY p."date") AS price_partition
+                          FROM
+                            (SELECT x."date",
+                                    t.price
+                             FROM
+                               (SELECT generate_series(min("date"), '{request.query_params['check_out']}'::date, '1 day')::date AS "date"
+                                FROM public.booking_roomprice
+                                where room_id = {pk}) x
+                             LEFT   JOIN public.booking_roomprice t USING ("date")
+                             ORDER  BY x."date") AS p
+                          ORDER  BY p."date") AS q) AS z
+                    WHERE "date" >= '{request.query_params['check_in']}'::date""")
+                row = cursor.fetchone()
+            except:
+                return Response("Something is wrong!", status=status.HTTP_400_BAD_REQUEST)
+        return Response({"price": row[0]})
 
 
 class RoomPriceViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
